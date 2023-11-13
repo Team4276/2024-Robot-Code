dc5e429a48dba78f8e4b03768a183e09c12837ce
AndyC <andrew.cappon@gmail.com>
2023-11-12 20:03:34 -0800

    First test, 80% power via TuffBox to pneumatic tire, with and without friction load

--- motor_control/src/main/java/frc/robot/Robot.java
+++ motor_control/src/main/java/frc/robot/Robot.java
@@ -4,7 +4,13 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.Counter;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.motorcontrol.VictorSP;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
@@ -13,43 +19,99 @@ import edu.wpi.first.wpilibj.TimedRobot;
  * project.
  */
 public class Robot extends TimedRobot {
+
+public double rpmSetPoint = 0.0;
+
+private VictorSP cimMotor;
+
+private PIDController cimPidController;
+private final double kP = 1.0;   
+private final double kI = 0.0;   
+private final double kD = 0.0;   
+
+private final int DIO_HALL_ENCODER = 0;
+private final int DIO_GO_BUTTON = 1;
+private final int DIO_USE_PID_JUMPER = 2;
+
+private Counter hallEncoder;
+private final double NUMBER_OF_TEETH_ON_HALL_ENCODER_SPROCKET = 13;
+
+private double rpmMeasured = 0.0;
+
+private DigitalInput goButton;
+private DigitalInput usePidJumper;
+
   /**
    * This function is run when the robot is first started up and should be used for any
    * initialization code.
    */
   @Override
-  public void robotInit() {}
+  public void robotInit() {
+    cimMotor = new VictorSP(9);
+    cimPidController = new PIDController(kP, kI, kD); 
+    hallEncoder = new Counter(DIO_HALL_ENCODER);
+    hallEncoder.setDistancePerPulse(360.0/NUMBER_OF_TEETH_ON_HALL_ENCODER_SPROCKET);  // Degrees
+    hallEncoder.reset();
+    goButton = new DigitalInput(DIO_GO_BUTTON);
+    usePidJumper = new DigitalInput(DIO_USE_PID_JUMPER);
+  }
 
   @Override
-  public void robotPeriodic() {}
+  public void robotPeriodic() {
+
+    rpmMeasured = hallEncoder.getRate();  // Degrees/sec
+     rpmMeasured *= 60.0;  // Degrees/min
+    rpmMeasured /= 360.0;  // RPM
+
+    SmartDashboard.putNumber("rpmSetPoint", rpmSetPoint);
+    SmartDashboard.putNumber("rpmMeasured", rpmMeasured);
+  }
 
   @Override
   public void autonomousInit() {}
 
   @Override
-  public void autonomousPeriodic() {}
+  public void autonomousPeriodic() {
+    cimMotor.set(0.0); 
+  }
 
   @Override
   public void teleopInit() {}
 
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {
+     if(goButton.get()) {
+      cimMotor.set(0.0);
+    } else {
+      if(usePidJumper.get()) {
+         cimMotor.set(cimPidController.calculate(rpmMeasured, rpmSetPoint));
+      } else {
+        cimMotor.set(0.8);
+      }
+    }
+  }
 
   @Override
   public void disabledInit() {}
 
   @Override
-  public void disabledPeriodic() {}
+  public void disabledPeriodic() {
+    cimMotor.set(0.0);
+  }
 
   @Override
   public void testInit() {}
 
   @Override
-  public void testPeriodic() {}
+  public void testPeriodic() {
+    cimMotor.set(0.0);
+  }
 
   @Override
   public void simulationInit() {}
 
   @Override
-  public void simulationPeriodic() {}
+  public void simulationPeriodic() {
+    cimMotor.set(0.0);
+  }
 }
