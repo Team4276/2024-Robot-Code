200f147ed7c4a941160f07968436a441347fc176
AndyC <andrew.cappon@gmail.com>
2023-12-05 14:17:56 -0800

    Change to using TalonSRX for controller, Added max RPM safety

--- motor_control/src/main/java/frc/robot/Robot.java
+++ motor_control/src/main/java/frc/robot/Robot.java
@@ -4,114 +4,144 @@
 
 package frc.robot;
 
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.TalonSRXControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.can.TalonSRXConfiguration;
+
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.Counter;
 import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.motorcontrol.VictorSP;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 /**
- * The VM is configured to automatically run this class, and to call the functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the name of this class or
- * the package after creating this project, you must also update the build.gradle file in the
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the
+ * name of this class or
+ * the package after creating this project, you must also update the
+ * build.gradle file in the
  * project.
  */
 public class Robot extends TimedRobot {
 
-public double rpmSetPoint = 0.0;
+  public double rpmSetPoint = 0.0;
 
-private VictorSP cimMotor;
+  private TalonSRX motor775Pro;
 
-private PIDController cimPidController;
-private final double kP = 1.0;   
-private final double kI = 0.0;   
-private final double kD = 0.0;   
+  private PIDController myPidController;
+  private final double kP = 1.0;
+  private final double kI = 0.0;
+  private final double kD = 0.0;
 
-private final int DIO_HALL_ENCODER = 0;
-private final int DIO_GO_BUTTON = 1;
-private final int DIO_USE_PID_JUMPER = 2;
+  private final int kSensorUnitsPerRotation_775Pro = 12;
 
-private Counter hallEncoder;
-private final double NUMBER_OF_TEETH_ON_HALL_ENCODER_SPROCKET = 13;
+  private final int DIO_GO_BUTTON = 0;
+  private final int DIO_USE_PID_JUMPER = 5;
+  private final int DIO_USE_PID_BRAKE = 6; // Normally brake mode, this will use PID to set RPM to zero - much faster
+                                           // stop
 
-private double rpmMeasured = 0.0;
+  private final int MAX_RPM = 120;
 
-private DigitalInput goButton;
-private DigitalInput usePidJumper;
+  private double rpmMeasured = 0.0;
+
+  private DigitalInput goButton;
+  private DigitalInput usePidSwitch;
+
+  private boolean safetySpinDown = false; // Gets set TRUE if max RPM is exceeded, resets to false when 'GO' button is
+                                          // released
 
   /**
-   * This function is run when the robot is first started up and should be used for any
+   * This function is run when the robot is first started up and should be used
+   * for any
    * initialization code.
    */
   @Override
   public void robotInit() {
-    cimMotor = new VictorSP(9);
-    cimPidController = new PIDController(kP, kI, kD); 
-    hallEncoder = new Counter(DIO_HALL_ENCODER);
-    hallEncoder.setDistancePerPulse(360.0/NUMBER_OF_TEETH_ON_HALL_ENCODER_SPROCKET);  // Degrees
-    hallEncoder.reset();
+    motor775Pro = new TalonSRX(9);
+    TalonSRXConfiguration myConfig = new TalonSRXConfiguration();
+    motor775Pro.configAllSettings(myConfig); // set defaults
+
+    motor775Pro.setNeutralMode(NeutralMode.Brake);
+
+    myPidController = new PIDController(kP, kI, kD);
     goButton = new DigitalInput(DIO_GO_BUTTON);
-    usePidJumper = new DigitalInput(DIO_USE_PID_JUMPER);
+    usePidSwitch = new DigitalInput(DIO_USE_PID_JUMPER);
   }
 
   @Override
   public void robotPeriodic() {
 
-    rpmMeasured = hallEncoder.getRate();  // Degrees/sec
-     rpmMeasured *= 60.0;  // Degrees/min
-    rpmMeasured /= 360.0;  // RPM
+    double rawSensorUnitsPer100ms = motor775Pro.getSelectedSensorVelocity();
+    rpmMeasured = (rawSensorUnitsPer100ms * (10.0/kSensorUnitsPerRotation_775Pro))  * 60.0;  // RPM
 
     SmartDashboard.putNumber("rpmSetPoint", rpmSetPoint);
     SmartDashboard.putNumber("rpmMeasured", rpmMeasured);
-  }
 
-  @Override
-  public void autonomousInit() {}
-
-  @Override
-  public void autonomousPeriodic() {
-    cimMotor.set(0.0); 
-  }
-
-  @Override
-  public void teleopInit() {}
-
-  @Override
-  public void teleopPeriodic() {
-     if(goButton.get()) {
-      cimMotor.set(0.0);
+    if (rpmMeasured > MAX_RPM) {
+      safetySpinDown = true;
     } else {
-      if(usePidJumper.get()) {
-         cimMotor.set(cimPidController.calculate(rpmMeasured, rpmSetPoint));
-      } else {
-        cimMotor.set(0.8);
+      if (goButton.get()) {
+        safetySpinDown = false;
       }
     }
   }
 
   @Override
-  public void disabledInit() {}
+  public void autonomousInit() {
+  }
+
+  @Override
+  public void autonomousPeriodic() {
+    motor775Pro.set(TalonSRXControlMode.PercentOutput, 0.0);
+  }
+
+  @Override
+  public void teleopInit() {
+  }
+
+  @Override
+  public void teleopPeriodic() {
+    if (safetySpinDown) {
+      motor775Pro.set(TalonSRXControlMode.PercentOutput, 0.0);
+    } else {
+      if (goButton.get()) {
+        motor775Pro.set(TalonSRXControlMode.PercentOutput, 0.0);
+      } else {
+        if (usePidSwitch.get()) {
+          motor775Pro.set(TalonSRXControlMode.PercentOutput, myPidController.calculate(rpmMeasured, rpmSetPoint));
+        } else {
+          motor775Pro.set(TalonSRXControlMode.PercentOutput, 0.8);
+        }
+      }
+    }
+  }
+
+  @Override
+  public void disabledInit() {
+  }
 
   @Override
   public void disabledPeriodic() {
-    cimMotor.set(0.0);
+    motor775Pro.set(TalonSRXControlMode.PercentOutput, 0.0);
   }
 
   @Override
-  public void testInit() {}
+  public void testInit() {
+  }
 
   @Override
   public void testPeriodic() {
-    cimMotor.set(0.0);
+    motor775Pro.set(TalonSRXControlMode.PercentOutput, 0.0);
   }
 
   @Override
-  public void simulationInit() {}
+  public void simulationInit() {
+  }
 
   @Override
   public void simulationPeriodic() {
-    cimMotor.set(0.0);
+    motor775Pro.set(TalonSRXControlMode.PercentOutput, 0.0);
   }
 }
