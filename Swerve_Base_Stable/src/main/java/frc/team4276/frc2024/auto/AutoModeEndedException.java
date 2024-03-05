fbd9fea7fc7f7f9237eb49801ee99582f5707a7f
Breadfest <ethanoskatt1@gmail.com>
2024-03-04 16:19:05 -0800

    move other projects to seperate branch

--- Swerve_Base_Stable/src/main/java/frc/team4276/frc2024/auto/AutoModeEndedException.java
+++
@@ -1,8 +0,0 @@
-package frc.team4276.frc2024.auto;
-
-/**
- * Exception thrown when an auto mode ends unexpectedly
- */
-public class AutoModeEndedException extends Exception {
-    private static final long serialVersionUID = 1411131586291540143L;
-}
