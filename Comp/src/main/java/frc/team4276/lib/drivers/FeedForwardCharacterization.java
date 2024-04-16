8b90d71112a535d7608c31e80eb1d008ee6ff9e8
Breadfest <ethanoskatt1@gmail.com>
2024-04-15 19:09:22 -0700

    begin work on lower level motor control

---
+++ Comp/src/main/java/frc/team4276/lib/drivers/FeedForwardCharacterization.java
@@ -0,0 +1,12 @@
+package frc.team4276.lib.drivers;
+
+/**
+ * WIP
+ * 
+ * <p> Interface for FeedForward characterizations
+ * 
+ * <p> What do I name this...
+ */
+public interface FeedForwardCharacterization {
+    
+}
