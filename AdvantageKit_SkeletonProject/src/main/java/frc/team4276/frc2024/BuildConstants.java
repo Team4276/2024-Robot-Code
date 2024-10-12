c8870edc67bdf0f744b39c14a9d86e17b713f19e
eBulosan <ethanoskatt1@gmail.com>
2024-10-11 23:10:10 -0700

    add build constants

---
+++ AdvantageKit_SkeletonProject/src/main/java/frc/team4276/frc2024/BuildConstants.java
@@ -0,0 +1,17 @@
+package frc.team4276.frc2024;
+
+/** Automatically generated file containing build version information. */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "AdvantageKit_SkeletonProject";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 358;
+  public static final String GIT_SHA = "fb530fcc6df91e5a9a50d105be4d6af8ab1dae00";
+  public static final String GIT_DATE = "2024-10-11 05:28:17 GMT";
+  public static final String GIT_BRANCH = "cmd-based";
+  public static final String BUILD_DATE = "2024-10-12 06:09:19 GMT";
+  public static final long BUILD_UNIX_TIME = 1728713359059L;
+  public static final int DIRTY = 0;
+
+  private BuildConstants() {}
+}
