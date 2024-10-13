8e2f53c1f40afd2b5d5b3ad71f0af4c3c2df13cd
eBulosan <ethanoskatt1@gmail.com>
2024-10-12 21:27:45 -0700

    remove version file creation on build

--- AdvantageKit_SkeletonProject/src/main/java/frc/team4276/frc2024/Robot.java
+++ AdvantageKit_SkeletonProject/src/main/java/frc/team4276/frc2024/Robot.java
@@ -40,8 +40,6 @@ public class Robot extends LoggedRobot {
    */
   @Override
   public void robotInit() {
-    recordMetaData();
-
     // Set up data receivers & replay source
     switch (Constants.currentMode) {
       case REAL:
@@ -73,25 +71,6 @@ public class Robot extends LoggedRobot {
     // Initialize auto chooser
     chooser.addDefaultOption("Default Auto", defaultAuto);
     chooser.addOption("My Auto", customAuto);
-  }
-
-  private void recordMetaData() {
-    Logger.recordMetadata("ProjectName", BuildConstants.MAVEN_NAME);
-    Logger.recordMetadata("BuildDate", BuildConstants.BUILD_DATE);
-    Logger.recordMetadata("GitSHA", BuildConstants.GIT_SHA);
-    Logger.recordMetadata("GitDate", BuildConstants.GIT_DATE);
-    Logger.recordMetadata("GitBranch", BuildConstants.GIT_BRANCH);
-    switch (BuildConstants.DIRTY) {
-      case 0:
-        Logger.recordMetadata("GitDirty", "All changes committed");
-        break;
-      case 1:
-        Logger.recordMetadata("GitDirty", "Uncomitted changes");
-        break;
-      default:
-        Logger.recordMetadata("GitDirty", "Unknown");
-        break;
-    }
   }
 
   /** This function is called periodically during all modes. */
