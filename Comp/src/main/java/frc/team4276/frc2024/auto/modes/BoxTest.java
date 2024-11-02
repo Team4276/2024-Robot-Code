55fe27890c8c0d73e966948044c62a5b4c9ef962
Breadfest <ethanoskatt1@gmail.com>
2024-11-02 09:33:41 -0700

    add test for old trajectory action

--- Comp/src/main/java/frc/team4276/frc2024/auto/modes/BoxTest.java
+++ Comp/src/main/java/frc/team4276/frc2024/auto/modes/BoxTest.java
@@ -3,13 +3,14 @@ package frc.team4276.frc2024.auto.modes;
 import frc.team254.lib.geometry.Pose2d;
 import frc.team4276.frc2024.auto.AutoModeBase;
 import frc.team4276.frc2024.auto.AutoModeEndedException;
-import frc.team4276.frc2024.auto.actions.ChoreoTrajectoryAction;
+// import frc.team4276.frc2024.auto.actions.ChoreoTrajectoryAction;
+import frc.team4276.frc2024.auto.actions.SwerveTrajectoryAction;
 
 public class BoxTest extends AutoModeBase {
-    private final ChoreoTrajectoryAction traj1;
+    private final SwerveTrajectoryAction traj1;
 
     public BoxTest(){
-        traj1 = new ChoreoTrajectoryAction("Test2");
+        traj1 = new SwerveTrajectoryAction("Test2");
     }
 
     @Override
